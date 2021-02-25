f324106fabeba40d819c6a10fffdf116271bf99f
ankitsharma1998 <ankit.sharma30061998.com>
2021-02-25 16:33:13 +0530

    maven assignment

---
+++ mavenassignment/module1/src/main/java/module1/newclassinmodule.java
@@ -0,0 +1,7 @@
+package module1;
+
+public class newclassinmodule {
+    public static void main(String[] args) {
+        System.out.println("This class is created inside a module");
+    }
+}
---
+++ mavenassignment/module2/src/main/java/module2/module2.java
@@ -0,0 +1,7 @@
+package module2;
+
+public class module2 {
+    public static void main(String[] args) {
+        System.out.println("this class is inside module 2");
+    }
+}
